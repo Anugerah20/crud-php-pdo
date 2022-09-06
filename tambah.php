1cbe909074211409538f29a3a9f158271a04001e
Nabil Anugerah <nabilpangestu123@gmail.com>
2022-09-06 17:48:09 +0700

    membuat tampilan halaman tambah barang

--- tambah.php
+++ tambah.php
@@ -2,3 +2,25 @@
 require_once('koneksi.php');
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+     <title>Tambah Barang</title>
+</head>
+<body>
+     <div class="container">
+          <div class="row">
+               <div class="col-lg-6">
+                    <form action="" method="POST">
+                         
+                    </form>
+               </div>
+          </div>
+     </div>
+</body>
+</html>
