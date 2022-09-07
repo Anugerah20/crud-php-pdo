02a8bcf216ea04e51d644cb386c76f4756efccd9
Nabil Anugerah <nabilpangestu123@gmail.com>
2022-09-07 08:59:02 +0700

    membuat hapus barang berdasarkan id

--- hapus.php
+++ hapus.php
@@ -2,6 +2,9 @@
 require_once('koneksi.php');
 
 // Menghapus data berdasarka id
-
+$id  = $_GET['id'];
+$sql = "DELETE FROM barang WHERE id_barang= ?";
+$row = $connect->prepare($sql);
+$row->execute(array($id));
 
 ?>
