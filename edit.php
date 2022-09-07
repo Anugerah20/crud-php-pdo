597c8fa11b7e7c255c97eb567e156a47044d5d03
Nabil Anugerah <nabilpangestu123@gmail.com>
2022-09-07 08:22:02 +0700

    membuat tampilan edit

--- edit.php
+++ edit.php
@@ -32,7 +32,7 @@ if(!empty($_POST['nama_barang'])) {
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-     <title>Tambah Barang</title>
+     <title>Edit Barang - <?php echo $get['nama_barang']; ?></title>
 </head>
 <body>
      <!-- START: Contact -->
