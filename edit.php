870e18fa3bf9e77b1a440b99c2291be94112faf6
Nabil Anugerah <nabilpangestu123@gmail.com>
2022-09-07 08:12:35 +0700

    menangkap data barang untuk di edit

--- edit.php
+++ edit.php
@@ -4,5 +4,9 @@ require_once('koneksi.php');
 // Proses Edit Barang
 if(!empty($_POST['nama_barang'])) {
      // Menangkap data barang
+     $nama_barang = $_POST['nama_barang'];
+     $stok = $_POST['stok'];
+     $harga = $_POST['harga'];
+     $tanggal = $_POST['tanggal'];
 }
 ?>
