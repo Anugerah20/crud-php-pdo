93535ccfb175e24f75c1d5a1ecaf5a771815c54b
Nabil Anugerah <nabilpangestu123@gmail.com>
2022-09-07 08:15:51 +0700

    menyimpan data barang yang sudah di edit

--- edit.php
+++ edit.php
@@ -8,5 +8,15 @@ if(!empty($_POST['nama_barang'])) {
      $stok = $_POST['stok'];
      $harga = $_POST['harga'];
      $tanggal = $_POST['tanggal'];
+
+     $data[] = $nama_barang;
+     $data[] = $stok;
+     $data[] = $harga;
+     $data[] = $tanggal;
+
+     // Simpan data barang
+     $sql = "UPDATE barang SET nama_barang=?,stok=?,harga_barang=?,tgl_masuk=? WHERE id_barang=?";
+     $row = $connect->prepare($sql);
+     $row->execute($data);
 }
 ?>
